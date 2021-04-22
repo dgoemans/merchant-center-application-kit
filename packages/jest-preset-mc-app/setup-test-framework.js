866411bcc46ecf397f98ee70af32a2b82db65597
tdeekens <tobias.deekens@commercetools.com>
2021-04-22 08:37:55 +0200

    refactor(jest-preset-mc-app): to allow for expect config

--- packages/jest-preset-mc-app/setup-test-framework.js
+++ packages/jest-preset-mc-app/setup-test-framework.js
@@ -3,10 +3,7 @@ require('jest-enzyme');
 require('@testing-library/jest-dom/extend-expect');
 require('./polyfills/intl');
 
-const ShallowWrapper = require('enzyme/ShallowWrapper');
 const { configure: configureRtl } = require('@testing-library/react');
-const configureEnzymeExtensions = require('@commercetools/enzyme-extensions');
-const commerceToolsEnzymeMatchers = require('@commercetools/jest-enzyme-matchers');
 const loadConfig = require('./load-config');
 
 const jestConfig = loadConfig();
@@ -17,15 +14,8 @@ if (jestConfig.rtlConfig) {
 if (jestConfig.enzymeConfig) {
   Enzyme.configure(jestConfig.enzymeConfig);
 }
-
-expect.extend({
-  toBeComponentWithName(received, actual) {
-    const pass = received && received.displayName === actual;
-    const message = () =>
-      `expected ${received} ${pass ? 'not ' : ''} to be ${actual}`;
-    return { message, pass };
-  },
-});
-
-expect.extend(commerceToolsEnzymeMatchers);
-configureEnzymeExtensions(ShallowWrapper);
+if (jestConfig.expectConfig) {
+  if (jestConfig.expectConfig.extend) {
+    expect.extend(jestConfig.expectConfig.extend);
+  }
+}
